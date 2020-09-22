dafc1d43227898f9961f9af6e3543ecff654a620
Nicholas Stepanov <stepko@inbox.ru>
2020-09-22 22:19:00 +0200

    Different Twitter Code verification procedure to avoid bot get stuck.

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const select = require ('puppeteer-select');
 const puppeteer = require('puppeteer');
 
 var useHash = false;
+var rollForHash = false;
 var myHash = "";
 
 async function autoScroll(page){
@@ -105,6 +106,7 @@ catch{
    document.getElementById('hash_res').innerText += 'Code unsuccesfull';
 }
   }
+  else{
 
   await autoScroll(page);
 
@@ -118,6 +120,7 @@ catch{
   const innerReward = await page.evaluate(() => document.querySelector('.result').innerText);
   var result = "<br>  Lucky Number: " + luckyNums + "<br>" + innerReward + "<br>" + balance;
   document.getElementById('results').innerHTML += '<br>' + result;
+}
 
 }
 catch(e){
@@ -131,9 +134,11 @@ loopError = true
 
   await page.close()
   await browser.close()
+  if(useHash){rollForHash = true;}
+  useHash = false;
+  if (!rollForHash){
   if (loopError){
   document.getElementById('results').innerHTML += '<br>Not all coins were collected.<br>Sleep for 10 minutes';
-  useHash = false;
   myHash = "";
   var counter = 0;
   while (counter < 600){
@@ -146,7 +151,6 @@ loopError = true
   } else{
   console.log('!!!!!!!!!!!!!!!!! All coins collected succesfully !!!!!!!!!!!!!!!!!!!!!!')
   document.getElementById('results').innerHTML += '<br>All coins were collected!<br>Sleep for an hour<br><br>';
-  useHash = false;
   myHash = "";
 
   var counter = 0;
@@ -159,6 +163,10 @@ loopError = true
   }
 
   }
+}
+else{
+  rollForHash = false;
+}
   document.getElementById('price').innerHTML = '';
 
 // while loop end
